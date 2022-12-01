abc20cd4bb15d43f90e282846977fae59210e04c
RiddhiDedhia98 <97441119+RiddhiDedhia98@users.noreply.github.com>
2022-12-01 12:50:55 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,23 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  "viewportWidth": 1200,
-  
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    supportFile: False,
-    setupNodeEvents(on, config) {
-      on('task', {
-        log(message) {
-          console.log(message)
-      
-          return null
-        },
-      })
-      return require('./cypress/plugins/index.js')(on, config);
-    }
-    
-
+    supportFile: False
   }
 });
