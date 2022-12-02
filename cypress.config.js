96250f9e50f7ddf9da944c0555c35f0749810607
Kavita Lalkiya <kla@salecto.in>
2022-12-02 15:10:59 +0530

    original code

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('cypress');
+
 module.exports = defineConfig({
   "viewportWidth": 1200,
+  
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
@@ -8,10 +10,13 @@ module.exports = defineConfig({
       on('task', {
         log(message) {
           console.log(message)
+      
           return null
         },
       })
-      return require('./cypress/Plugins/index.js')(on, config);
+      return require('./cypress/plugins/index.js')(on, config);
     }
+    
+
   }
 });
--- cypress/e2e/myFirst.cy.js
+++ cypress/e2e/myFirst.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
     it('home page', function(){
         cy.visit('https://smaakryb.stag2.salecto.dk/artingarden')
+        cy.wait(15000)
 
     })
